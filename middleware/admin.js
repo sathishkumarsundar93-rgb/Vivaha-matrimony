c4356fcd9682589db75422a28e1c2d891e680582
sathishkumarsundar93-rgb <sathishkumarsundar93@gmail.com>
2026-03-23 21:42:06 +0530

    Add admin role middleware for access control

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,1 +1,8 @@
 
+module.exports = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403).json({ msg: 'Admin access required' });
+  }
+};
